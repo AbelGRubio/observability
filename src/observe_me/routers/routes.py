dd70a05e6158a4de54c8dc65f055048215c99bd2
AbelGRubio <ablg11673@gmail.com>
2026-03-28 11:48:53 +0100

    feat: loki configuration works

--- src/observe_me/routers/routes.py
+++ src/observe_me/routers/routes.py
@@ -5,9 +5,10 @@ from fastapi.responses import JSONResponse
 from opentelemetry import trace
 
 from observe_me.config import __version__
+from observe_me.core.logger_api import get_logger
 
 v1_router = APIRouter()
-
+logger = get_logger(__name__)
 
 @v1_router.get("/route")
 def route(name: str) -> JSONResponse:
@@ -21,4 +22,5 @@ def route(name: str) -> JSONResponse:
         user_id = 123
 
         span.add_event("User processed", {"user.id": f"{name}:{user_id}", "result": "ok"})
+    logger.info("Doing things here")
     return JSONResponse(content={f"{name}, the version is": __version__}, status_code=status_code)
