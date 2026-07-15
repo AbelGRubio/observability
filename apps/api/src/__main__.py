1cdd30846cb47e2221e4e4b41251d9514c1bd0d0
Abel García <46522725+AbelGRubio@users.noreply.github.com>
2026-07-15 11:00:10 +0200

    fix(api): update main api

--- apps/api/src/__main__.py
+++ apps/api/src/__main__.py
@@ -1,22 +1,16 @@
 """Entry point."""
 
-import logging
-
 import uvicorn
+from observe_core.logger import get_logger, propague_loggers
+
 from observe_api import configure_app, define_app, get_app_settings
-from observe_core.logger import get_logger
 
 logger = get_logger(__name__)
 
 configure_app()
 app = define_app()
 
-# To put all loggers in same format.
-for name in ("uvicorn", "uvicorn.error", "uvicorn.access"):
-    uvicorn_logger = logging.getLogger(name)
-    uvicorn_logger.handlers = logger.handlers
-    uvicorn_logger.setLevel(logging.DEBUG)
-    uvicorn_logger.propagate = False
+propague_loggers()
 
 
 if __name__ == "__main__":
